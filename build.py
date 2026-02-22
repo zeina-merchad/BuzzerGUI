1a2f0cb891be270a7efd000dddb739e1a04e9b31
zeina mershad <mershadzeina7@gmail.com>
2026-02-22 09:30:56 +0200

    savepoint

---
+++ build.py
@@ -0,0 +1,35 @@
+import PyInstaller.__main__
+import os
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+
+PyInstaller.__main__.run([
+    'main.py',
+    '--onedir',
+    '--noconsole',
+    '--name=FootballQuiz',
+    '--clean',
+    f'--add-data={ROOT}/assets:assets',
+    f'--add-data={ROOT}/app:app',
+    f'--add-data={ROOT}/app/ui/screens/logo.png:app/ui/screens',
+    f'--add-data={ROOT}/app/ui/screens/logo_full.png:app/ui/screens',
+    '--collect-all=PySide6',
+    '--collect-all=paho',
+    '--hidden-import=app.ui.app_window',
+    '--hidden-import=app.ui.screens.admin_dashboard',
+    '--hidden-import=app.ui.screens.host_screen',
+    '--hidden-import=app.ui.screens.round_transition_screen',
+    '--hidden-import=app.ui.screens.winner_screen',
+    '--hidden-import=app.ui.widgets',
+    '--hidden-import=app.ui.key_discovery',
+    '--hidden-import=app.ui.remote_config',
+    '--hidden-import=app.config',
+    '--hidden-import=app.core.engine',
+    '--hidden-import=app.core.models',
+    '--hidden-import=app.hardware.mqtt_buzzer',
+    '--hidden-import=app.io',
+    '--hidden-import=app.sim',
+    '--hidden-import=app.constants',
+    '--exclude-module=tkinter',
+    '--exclude-module=matplotlib',
+])
